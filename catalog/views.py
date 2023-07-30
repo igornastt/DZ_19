ae4783c64d9f92337c79141433244150e883f244
Igor <100830659+igornastt@users.noreply.github.com>
2023-07-30 08:38:21 +0300

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'catalog/index.html')
 
-def contacts(request):
+def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
