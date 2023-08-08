cf8372921566fad10c0c5221cbd45323cea9e5cd
Igor <100830659+igornastt@users.noreply.github.com>
2023-08-08 06:26:29 +0300

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from . import views
 from catalog.views import index, contact
 
 urlpatterns = [
-    path('', index),
-    path('contacts/', contact)
+    path('', views.index, name='index'),
+    path('contacts/', contact),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
