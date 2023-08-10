ee49cc50d077ec872ebf6ffca9993119b5ba7749
igornast <grezle00@gmail.com>
2023-08-10 14:57:03 +0300

    Создание файлов

---
+++ catalog/migrations/0002_category.py
@@ -0,0 +1,18 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('catalog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=150, verbose_name='наименование')),
+                ('text', models.TextField(max_length=10000, verbose_name='описание')),
+            ],
+        ),
+    ]
